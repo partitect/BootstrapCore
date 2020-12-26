93893790f5fa175750068c2d8c4893b6c9b003a9
Telat Kaya <telatkaya@yandex.com>
2020-12-26 21:17:55 +0300

    Proje dosyası ekle.

---
+++ BootstrapCore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using BootstrapCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapCore.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			Albums albums = new Albums { id = 1, title = "Kolonya", desc= "",imgPath="" };
+			return View(albums);
+		}
+	}
+}
